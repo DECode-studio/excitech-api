855ab8d087c2bbb8e8386e4f1547ccc890f866ca
DECode-studio <decode.std@gmail.com>
2023-04-04 10:07:22 +0700

    add prod ipaymu controller

--- api/controller/i-paymu/prod/balance/index.js
+++ api/controller/i-paymu/prod/balance/index.js
@@ -1,9 +1,9 @@
-const { devVa } = require('../../../../config/payment_gateway/pg_config')
+const { prodVa } = require('../../../../config/payment_gateway/pg_config')
 const GetBalance = require("./get/get_balance")
 
 const GetMethod = async (req, res) => {
     let data = new FormData();
-    data.append('account', devVa);
+    data.append('account', prodVa);
 
     return await GetBalance(data, res)
 }
