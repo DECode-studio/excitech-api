254215113df8ab5a66b9b8ef0b3e58f40d211280
DECode-studio <decode.std@gmail.com>
2023-04-06 11:06:57 +0700

    add my finance server key

--- api/config/fcm/fcm_my_finance.js
+++ api/config/fcm/fcm_my_finance.js
@@ -1,6 +1,6 @@
 const FCM = require('fcm-node');
-const { myFinanceConfig } = require('../firebase/init_my_finance')
 
-const fcmMyFinance = new FCM(myFinanceConfig.apiKey);
+const serverKey = 'AAAAJeUwP4k:APA91bHRrol7aTbhdvnX4K3hQP8uAT3aqtOP2aUD-ZxSQ05gap8cWViIIzHxGeveM7N4vUnKTMmH5uSI-JKYMDOFhQ9RHtLRSQgMtLmN7ou8HWQueWIlK8z9vHTnHsAhvl5nC_RzUNy4'
+const fcmMyFinance = new FCM(serverKey);
 
 module.exports = fcmMyFinance
