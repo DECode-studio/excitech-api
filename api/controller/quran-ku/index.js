1e407052facfdd83cb6f856c5dd965a833bdfc61
DECode-studio <decode.std@gmail.com>
2023-04-22 07:17:13 +0700

    add transaction quran ku routes

--- api/controller/quran-ku/index.js
+++ api/controller/quran-ku/index.js
@@ -1,7 +1,9 @@
 const status = require('./status')
 const users = require('./user')
+const transaction = require('./transaction')
 
 module.exports = {
     status,
     users,
+    transaction,
 }
--- api/routes/router/quran_ku_route.js
+++ api/routes/router/quran_ku_route.js
@@ -3,6 +3,8 @@ const controller = require('../../controller')
 module.exports = (app) => {
     app.route('/api/quran-ku/status')
         .post(controller.quran_ku.status.PostStatus)
+    app.route('/api/quran-ku/transaction')
+        .post(controller.quran_ku.transaction.PostMethod)
     app.route('/api/quran-ku/user')
         .get(controller.quran_ku.users.GetUsers)
 }
